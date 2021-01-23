3f9647a904961c9d67d54140416e714c5e993429
youngsuk-kim <thayer2392@gmail.com>
2021-01-23 22:44:23 +0900

    initial commit

---
+++ src/main/java/com/youngsuk/justbook/JustbookApplication.java
@@ -0,0 +1,12 @@
+package com.youngsuk.justbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JustbookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JustbookApplication.class, args);
+    }
+}
---
+++ src/test/java/com/youngsuk/justbook/JustbookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.youngsuk.justbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JustbookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
